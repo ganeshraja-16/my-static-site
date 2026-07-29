f3af7d44b8605f4dbb2415c873727b1f6e4e9e5c
ganeshraja-16 <ganeshraja.r16@gmail.com>
2026-07-29 18:21:29 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="container">
+      <h1>GitHub OIDC → Amazon ECS</h1>
+      <p>React app deployed using GitHub Actions</p>
+      <p>Docker Multi-stage Build</p>
+      <p>Amazon ECR → ECS EC2</p>
+    </div>
+  );
+}
